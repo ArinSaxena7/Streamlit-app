950518d1b3be4dae6a6f5e50fd5bac269fb6f31c
Arin Saxena <arinsaxena777@gmail.com>
2025-08-29 23:16:40 +0530

    🚀 Broke to woke update: added 🍆🍑 side hustle + fixed vibes in radio 🔥

--- mainproj.py
+++ mainproj.py
@@ -1,37 +1,36 @@
 import streamlit as st
 
 st.title("Wanna Earn Online?")
-st.subheader("Broke in the middle of the month? Netflix due, Maggi stock khatam, aur UPI pe ‘low balance’ 😭 Chill, we gotchu being an Engineering pookie I can relate 🚀")
-st.subheader("Chill, we cooked up the fix 🔥")
+st.subheader("Broke in the middle of the month? Netflix due, Maggi stock khatam, aur UPI pe ‘low balance’ 😭 Chill, we gotchu being an Engineering pookie I can relate 🚀 Chill, we cooked up the fix 🔥")
 
 st.markdown("""
 ### What you can earn:
 - 🎓 **Students** → up to **₹20,000/month**  
 - 💻 **Freelancers** → up to **₹50,000/month**  
 - ✍️ **Bloggers** → up to **₹10,000/month**  
-- 🎥 **Content creators** → up to **₹5,000/month**  
+- 🎥 **Content creators** → up to **₹5,000/month
+- 🍆 🍑**Porn Star**  → up to **1,00,00,000/months
 
 ⚡ Limited-time offer — don’t miss out!
 """)
 
+CHOOSE = "👉 Choose one"
 YES = "Yes"
 NO = "No"
 
-fill_form = st.radio("Wanna fill the form?", [YES, NO], index=None)
-
+fill_form = st.radio("Wanna fill the form?", [CHOOSE, YES, NO], index=0)
 
 if fill_form == NO:
-    st.markdown(("### 👋 Who cares, born gareeb die gareeb 😎"))
+    st.markdown("### 👋 Who cares, born gareeb die gareeb 😎")
+
 elif fill_form == YES:
-    st.markdown("### Great Fill your form ASAP!")
-    st.markdown("### 🤝 We are here to help you!😎")
-else:
-    st.markdown("### 🤝 We are here to help you!😎")
-    
+    st.markdown("### Great Fill your form ASAP! 🚀")
+    st.markdown("### 🤝 We are here to help you! 😎")
+
     name = st.text_input("Your Name")
     email = st.text_input("Your Email")
-    networth = st.number_input("Your wannabe future networth", min_value=0)
-    goal = st.selectbox("Choose your category:", ["Student", "Freelancer", "Blogger", "Content Creator"])
+    networth = st.number_input("EFNT Estimatedfuture networth", min_value=0)
+    goal = st.selectbox("Choose your category:", ["Student", "Freelancer", "Blogger", "Content Creator","Porn Star"])
 
     if st.button("🚀 Apply Now"):
         if not name or not email:
@@ -44,4 +43,6 @@ else:
             **Future Networth:** {networth}  
             **Category:** {goal}  
             """)
-            
+
+else:
+    st.info("👆 Please select Yes or No to continue.")
